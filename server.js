ad8837dc66e894d666bf2224683aa7cb87b03db9
Abdulla <a.alkawai@gmail.com>
2017-01-10 20:08:23 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 
 //create application
 var app = express(); //access to exxpress api
+//Environment variable
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }
+  else{
+    resp.redirect('http://' + req.hostname + req.url)
+  }
+});
 
 //to add functionality to the app
 app.use(express.static('public'));
@@ -9,6 +20,6 @@ app.use(express.static('public'));
 //server
 // prm 1 = port number
 // prm2 = function
-app.listen(3000, function(){
-  console.log('server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('server is up on port ' + PORT);
 });
